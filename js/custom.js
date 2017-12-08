6e98b3a72d4c783d9b59b85df2d4a9905ca73484
rk <471091127@qq.com>
2017-12-08 23:04:34 +0800

    custom

--- js/custom.js
+++ js/custom.js
@@ -6,39 +6,58 @@
 			document.body.appendChild(el)
 		},
 		hide:function(){
-			document.body.removeChild(document.querySelector('.loading'))
+			var el=document.querySelector('.loading')
+			if(el){
+				document.body.removeChild(el)
+			}
 		}
 	}
-	loading.show()
-	var req=new XMLHttpRequest();	 
-	req.open("get","./content.json",false)
-	req.send()
-	if (req.readyState==4 && req.status==200){
-		   loading.hide()
-           var res=JSON.parse(req.responseText).data;
-           var part=document.querySelector("#main-content");
-           res.forEach(function(item){
-           	var content="<div class='main-right-item'>";
-           		content+="<div class='item-content'>",
-           		content+="<div class='user'><a href='"+item.userUrl+"'><img src='"+item.userPic+"'></a></div>",
-           		content+="<div class='detail'><div class='title'><a class='item-link' href='"+item.url+"'>"+item.title+"</a></div><div class='text'>"+item.text+"<a class='item-link' href='"+item.url+"'>详细...</a></div></div>",
-           		content+="</div>",
-           		content+="<div class='item-sign'>";
-           	item.sign.forEach(function(val){
-           		content+=sign(val);
-           	})
-           	content+="</div></div>";
-           	part.innerHTML+=content;
-           })
-        }else{
-        	loading.hide()
-        	console.log("error")
-        }  
+	router()	
+	function router(){
+		loading.show();
+		window.scrollTo(0,0);		
+		var hash=location.hash;
+		var req=new XMLHttpRequest();	
+		if(hash===""){			 
+			req.open("get","./content.json",false)
+			req.send()
+			if (req.readyState==4 && req.status==200){
+	           var res=JSON.parse(req.responseText).data;
+	           var part=document.querySelector("#main-content");
+	           part.innerHTML="";
+	           res.forEach(function(item){
+	           	var content="<div class='main-right-item'>";
+	           		content+="<div class='item-content'>",
+	           		content+="<div class='user'><a href='"+item.userUrl+"'><img src='"+item.userPic+"'></a></div>",
+	           		content+="<div class='detail'><div class='title'><a class='item-link' href='"+item.url+"'>"+item.title+"</a></div><div class='text'>"+item.text+"<a class='item-link' href='"+item.url+"'>详细...</a></div></div>",
+	           		content+="</div>",
+	           		content+="<div class='item-sign'>";
+	           		item.sign.forEach(function(val){
+	           			content+=sign(val);
+	           		})
+	           	content+="</div></div>";
+	           	part.innerHTML+=content;
+	           	loading.hide()
+	           })
+	        }else{
+	        	loading.hide()
+	        	console.log("error")
+	        }  
+		}else{
+			loading.hide()
+			let url="./"+hash.substring(1)+".md"; 
+		    req.open("get",url,false)
+			req.send()
+			document.getElementById('main-content').innerHTML="<div class='main-right-article'>"+marked(req.responseText)+"</div>"
+		}
+	}
     function sign(str){
     	switch(str){
     		case "js":return "<button class='sign-default sign-js'>javascript</button>";
     		break;
     		case "css":return "<button class='sign-default sign-css'>css</button>";
+    		break;
+    		case "self":return "<button class='sign-default sign-self'>原创</button>";
     		break;
     		case "copy":return "<button class='sign-default sign-copy'>转载</button>";
     		break;
@@ -54,23 +73,8 @@
    			this.className="main-right-item"
    		})
    	}
-   	document.getElementById("mark").onclick=function(){
-   		let req=new XMLHttpRequest();	 
-	    req.open("get","./html/example.md",false)
-		req.send()
-		document.getElementById('main-content').innerHTML="<div class='main-right-article'>"+marked(req.responseText)+"</div>"
-   	}
-   	var link=document.querySelectorAll(".item-link")
-   	for(let i=0;i<link.length;i++){
-   		link[i].onclick=function(e){
-   			let url="./"+this.getAttribute('href').substring(1)+".md";
-   			let req=new XMLHttpRequest();	 
-		    req.open("get",url,false)
-			req.send()
-			document.getElementById('main-content').innerHTML="<div class='main-right-article'>"+marked(req.responseText)+"</div>"
-   		}
-   	}
    	window.addEventListener("hashchange",function(){
    		console.log(location.hash)
+   		router()
    	})
 })()
